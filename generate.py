452a07bb58bf14dc807def3fbf0ad7ea4748a0dd
slanton06 <tohatohariziy@gmail.com>
2025-06-03 12:40:19 +0300

    upd

--- generate.py
+++ generate.py
@@ -4,9 +4,7 @@ import numpy as np
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('gests\sign_mnist_train.csv')
-
-# метки и пиксельные данные
+data = pd.read_csv('gests/sign_mnist_train.csv')
 labels = data['label'].values
 pixels = data.drop('label', axis=1).values
 
@@ -16,21 +14,44 @@ labels = to_categorical(labels)
 
 x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-    tf.keras.layers.MaxPooling2D(2, 2), # decreasing size and saving imp feat
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1,
+    horizontal_flip=True
+)
+datagen.fit(x_train)
 
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(0.3),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(0.3),
 
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'), # get all the features and turn it into final results
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dropout(0.4),
+
     tf.keras.layers.Dense(labels.shape[1], activation='softmax')
 ])
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10)
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+lr_callback = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
+
+model.fit(datagen.flow(x_train, y_train, batch_size=32),
+          validation_data=(x_test, y_test),
+          epochs=20,
+          callbacks=[early_stop, lr_callback])
 
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f'Точность модели: {accuracy * 100:.2f}%')
